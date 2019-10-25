e4b2b608bf43f3f8a625049f6cfc422c109522d7
Greyvin Tercero Ruiz <53482798+Origamy4321@users.noreply.github.com>
2019-10-24 20:15:42 -0500

    Se termino la lista Salon

--- Proyecto-Fase-Beta/listaSalon.cpp
+++ Proyecto-Fase-Beta/listaSalon.cpp
@@ -1,1 +1,66 @@
 #include "listaSalon.h"
+
+listaSalon::listaSalon() {
+
+	primero = nullptr;
+	actual = nullptr;
+}
+
+
+listaSalon::~listaSalon() {
+
+	while (primero != nullptr) {
+
+		actual = primero;
+		primero = primero->getSiguiente();
+		delete(actual);
+	}
+}
+
+void listaSalon::insertarInicio(Salon& salon) {
+
+	actual = new nodoSalon(salon, NULL);
+	if (primero == nullptr) {
+
+		primero = actual;
+	}
+	else {
+		actual->setSiguiente(primero);
+		primero = actual;
+	}
+}
+
+string listaSalon::toString() {
+
+	string resultado;
+	actual = primero;
+
+	while (actual != nullptr) {
+		resultado += actual->toString();
+		actual = actual->getSiguiente();
+	}
+
+	return resultado;
+
+}
+
+void listaSalon::setActual(nodoSalon*_actual) {
+
+	actual = _actual;
+
+}
+void listaSalon::setPrimer(nodoSalon* primer) {
+
+	primero = primer;
+
+}
+
+nodoSalon* listaSalon::getActual() {
+
+	return actual;
+}
+
+nodoSalon* listaSalon::getPrimero() {
+
+	return primero;
+}
--- Proyecto-Fase-Beta/listaSalon.h
+++ Proyecto-Fase-Beta/listaSalon.h
@@ -3,5 +3,28 @@
 
 class listaSalon
 {
+	nodoSalon* primero;
+	nodoSalon* actual;
+
+public:
+
+	listaSalon();
+
+	virtual ~listaSalon();
+
+	void insertarInicio(Salon& salon);
+
+	int totalNodos();
+
+	nodoSalon* getPrimero();
+
+	void setPrimer(nodoSalon* primero);
+
+	nodoSalon* getActual();
+
+	void setActual(nodoSalon* actual);
+
+	string toString();
+
 };
 
